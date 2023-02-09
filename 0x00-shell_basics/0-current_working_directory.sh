e77da0055c66cce2e2c245483ddbcb7a55ebbd1b
Joshua Ochoyi <joshuajrochoyi@gmail.com>
2023-02-08 22:45:55 -0800

    Print the path of current working directory

--- 0x00-shell_basics/0-current_working_directory.sh
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-
-
 # Get the current working directory
-
 cwd=$(pwd)
-
-
-
 # Print the absolute path of the current working directory
-
-echo "The absolute path of the current working directory is: $cwd"
+echo $cwd
